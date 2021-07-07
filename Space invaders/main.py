97a9e4b71d578b2ff1f217dced806177f716231c
br0steen <osteensco@gmail.com>
2021-07-07 17:09:01 -0400

    space invaders - added level transitions and messages, working on incorporating drops, commented out settings menu, adjusted asset sizes
    Tictactoe - added checks for move placement and fixed various gameplay issues. game is fully functional for two players.

--- Space invaders/main.py
+++ Space invaders/main.py
@@ -9,12 +9,12 @@ pygame.font.init()
 #__.blit places something in the window. in this game WIN is our window so you'll see objects placed by WIN.blit
 
 #TO DO:
-#______
-#______make a more robust start menu
-#______separate enemy laser vel and play laser vel
+#______make a more robust start menu with settings, etc
 #______enemy lasers dont disappear after enemies die
 #______add power ups, health, shields, etc
 #______add messages as things happen or levels/lives gained, lives lost, etc
+#______move everything into classes for better organization
+
 
 #game window
 WIDTH, HEIGHT = 1080, 650
@@ -23,22 +23,76 @@ pygame.display.set_caption("Space Invaders!")
 
 
 #Load images
+#func to load images
+def sprite(folder, image_name):
+    return pygame.image.load(os.path.join(folder, image_name))
+
+
+#drops
+health_drop = sprite("assets", "health_drop.png")
+
 #ships
-red_space_ship = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
-green_space_ship = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
-blue_space_ship = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
+red_space_ship = sprite("assets", "pixel_ship_red_small.png")
+green_space_ship = sprite("assets", "pixel_ship_green_small.png")
+blue_space_ship = sprite("assets", "pixel_ship_blue_small.png")
 
 #player ship
-player_space_ship = pygame.image.load(os.path.join("assets", "pixel_ship_yellow.png"))
+player_space_ship = sprite("assets", "pixel_ship_yellow.png")
 
 #Lasers
-red_laser = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
-green_laser = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
-blue_laser = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
-yellow_laser = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
+red_laser = sprite("assets", "pixel_laser_red.png")
+green_laser = sprite("assets", "pixel_laser_green.png")
+blue_laser = sprite("assets", "pixel_laser_blue.png")
+yellow_laser = sprite("assets", "pixel_laser_yellow.png")
 
 #background
-Background = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
+Background = pygame.transform.scale(sprite("assets", "background-black.png"), (WIDTH, HEIGHT))
+
+#menu and in game messaging font
+title_font = pygame.font.SysFont("comicsans", 60)
+
+
+#drop effects as functions, move to drop class??
+def health(obj):
+    if obj.health < 100:
+        obj.health = 100
+    else:
+        obj.lives += 1
+
+
+
+def drop_power(obj):
+    if random.randint(1, 3) == 1:
+        # need to add drop map similar to color map in enemy
+        drop = Drop(obj.x, obj.y, random.choice("health"))
+        drops.append(drop)
+#______CLASSES_______________________________________________________________________________________
+
+class Drop:
+    DROP_MAP = {
+                "health": (health, health_drop)
+                }
+
+    def __init__(self, x, y, power):
+        self.x = x
+        self.y = y
+        self.effect, self.img = DROP_MAP[power]
+        self.mask = pygame.mask.from_surface(self.img)
+
+    def draw(self, window):
+        window.blit(self.img, (self.x, self.y))
+
+    def move(self, vel):
+        self.y += vel
+
+    def collision(self, obj):
+        return collide(self, obj)
+
+    #find a way to connect the effect to a specific function (health example is below after collide and before main loop)
+
+
+
+#_____________________________________________________________________________________________
 
 class Laser:
     def __init__(self, x, y, img):
@@ -59,11 +113,10 @@ class Laser:
     def collision(self, obj):
         return collide(self, obj)
 
-
-
+#_____________________________________________________________________________________________
 
 class Ship:
-    COOLDOWN = 25
+    COOLDOWN = 30
 
     def __init__(self, x, y, health=100):
         self.x = x
@@ -71,8 +124,10 @@ class Ship:
         self.health = health
         self.ship_img = None
         self.laser_img = None
-        self.lasers = []
+        self.lasers = []#because lasers list is in class, lasers will disappear when ship is destroyed
         self.cool_down_counter = 0
+        self.drops = []
+        self.drop_img = None
 
     def draw(self, window):
         window.blit(self.ship_img, (self.x, self.y))
@@ -89,6 +144,12 @@ class Ship:
                 obj.health -= 10
                 self.lasers.remove(laser)
 
+
+
+    # def move_drops(self, vel, obj):    #move this to player class???
+    #     drop.move(vel)
+    #     if drop.collision(obj):
+    #         effect()
 
 
     def cooldown(self):
@@ -109,13 +170,18 @@ class Ship:
     def get_height(self):
         return self.ship_img.get_height()
 
+#_____________________________________________________________________________________________
+
 class Player(Ship):
+
     def __init__(self, x, y, health=100):
         super().__init__(x, y, health)
         self.ship_img = player_space_ship
         self.laser_img = yellow_laser
         self.mask = pygame.mask.from_surface(self.ship_img)
         self.max_health = health
+        self.drops = []
+
 
     def move_lasers(self, vel, objs):
         self.cooldown()
@@ -126,7 +192,8 @@ class Player(Ship):
             else:
                 for obj in objs:
                     if laser.collision(obj):#note this is different than move lasers in Ship class. Enemy ships don't have halth right now.
-                        objs.remove(obj)
+                        objs.remove(obj)#add drops below here. need another if statement for boss drops.
+                        drop_power(obj)
                         if laser in self.lasers:
                             self.lasers.remove(laser)
 
@@ -134,10 +201,11 @@ class Player(Ship):
         super().draw(window)
         self.healthbar(window)
 
-
-    def healthbar(self, window):
+    def healthbar(self, window):#player has a healthbar, might move this to ship so bosses can use?
             pygame.draw.rect(window, (255, 0, 0), (self.x, self.y + self.ship_img.get_height() + 10,self.ship_img.get_width(), 10))
             pygame.draw.rect(window, (0, 255, 0), (self.x, self.y + self.ship_img.get_height() + 10,self.ship_img.get_width() * (self.health / self.max_health), 10))
+
+#_____________________________________________________________________________________________
 
 class Enemy(Ship):
     COLOR_MAP = {
@@ -147,14 +215,21 @@ class Enemy(Ship):
                 }
 
     def __init__(self, x, y, color, health=100):
+        self.vel = 1
+        self.direction = True
         super().__init__(x, y, health)
         self.ship_img, self.laser_img = self.COLOR_MAP[color]
         self.mask = pygame.mask.from_surface(self.ship_img)
 
-    def move(self, vel):
-        self.y += vel
-
-
+    def move(self, vel):#for random side-side movement included add in parallel
+        if self.direction:#set parameters for changing vertical direction, has to be in class for individual movement
+            self.y += vel
+        if not self.direction:
+            self.y -= vel*2
+        # if random.randrange(1, 10) > 8 and self.x + self.get_width() < WIDTH:
+        #     self.x += parallel
+        # elif random.randrange(1, 10) > 6 and self.x > -1:
+        #     self.x -= parallel
 
 def collide(obj1, obj2):
     offset_x = obj2.x - obj1.x
@@ -162,6 +237,12 @@ def collide(obj1, obj2):
     return obj1.mask.overlap(obj2.mask, (offset_x, offset_y)) != None
 
 
+
+
+
+#_____________________________________________________________________________________________
+
+#game loop
 def main_loop():
     run = True
     FPS = 90
@@ -170,12 +251,16 @@ def main_loop():
     main_font = pygame.font.SysFont("comicsans", 50)
     lost_font = pygame.font.SysFont("comicsans", 100)
 
+    drops = []#initialize similar to how enemies spawn (in game loop), but trigger on enemy removal.
+    #actions of drops (movement, effect functions) should live in the drop class
+    #like enemies in some ways, lasers in others????
     enemies = []
-    wave_length = 5
-    enemy_vel = 20
+    wave_length = 3
+    # enparmove = round(enemy.vel*1.2)
+    enemy_laser_vel = 4
     laser_vel = 20
 
-    player_vel = 15#___variable to determine how many pixels per keystroke player moves
+    player_vel = 10#___variable to determine how many pixels per keystroke player moves
 
     player = Player(int(WIDTH/2) - int(player_space_ship.get_width()/2), HEIGHT - player_space_ship.get_height() - 20)#adjusted player position to be dynamic to window and ship size
 
@@ -183,16 +268,17 @@ def main_loop():
 
     lost = False
     lost_count = 0
+    transition_count = 0
 
     def redraw_window():
         WIN.blit(Background, (0,0))#background is anchored to top left corner
         #draw text
         if lives == 1:
-            lives_label = main_font.render(f"Lives: {lives}", 1, (255,0,0))
+            lives_label = main_font.render(f"Lives: {lives}", 1, (255,0,0))#lives label top left corner, red for 1 life left
         else:
-            lives_label = main_font.render(f"Lives: {lives}", 1, (255, 255, 255))
+            lives_label = main_font.render(f"Lives: {lives}", 1, (255, 255, 255))#lives label top left corner
 
-        level_label = main_font.render(f"Level: {level}", 1, (255,255,255))
+        level_label = main_font.render(f"Level: {level}", 1, (255,255,255))#level label top right corner
 
         WIN.blit(lives_label, (10, 10))
         WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))#accounts for label top left corner, we want this on the right side
@@ -201,6 +287,10 @@ def main_loop():
             enemy.draw(WIN)
 
         player.draw(WIN)
+
+        if len(enemies) == 0 and lives > 0 and level > 0:
+            level_label = title_font.render(f"Level Complete! Wave {level+1} incoming!", 1, (255, 255, 255))
+            WIN.blit(level_label, (WIDTH / 2 - level_label.get_width() / 2, HEIGHT / 2 - level_label.get_height() / 2))
 
         if lost:
             lost_label = lost_font.render("GAME OVER", 1, (255,0,0))
@@ -213,81 +303,117 @@ def main_loop():
         clock.tick(FPS)#this tells the game how fast to run, set by FPS variable
         redraw_window()
 
-
-        if lives <= 0:
-            lost = True
-            lost_count += 1
-
-        if lost:
-            if lost_count > FPS * 5:
-                run = False
-            else:
-                continue
-
-#spawn enemy ships
-        if len(enemies) == 0:#add a pause on next line and message that indicates level movement
-            level += 1
-            wave_length += level
-            for i in range(wave_length):
-                enemy = Enemy(random.randrange(50, WIDTH-100), random.randrange(-1500, 10), random.choice(["red", "blue", "green"]))
-                enemies.append(enemy)
-
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 quit()
-#________movement block, outside of event loop so that movement is less clunky
-        keys = pygame.key.get_pressed()#creates a variable to track key presses, checks based on FPS value. This block is where controls live.
-        if keys[pygame.K_a] and player.x > -1: #left movement
+        # ________movement block, outside of event loop so that movement is less clunky
+        keys = pygame.key.get_pressed()  # creates a variable to track key presses, checks based on FPS value. This block is where controls live.
+        if keys[pygame.K_a] and player.x > -1:  # left movement
             player.x -= player_vel
-        if keys[pygame.K_d] and player.x + player.get_width() < WIDTH: #right movement
+        if keys[pygame.K_d] and player.x + player.get_width() < WIDTH:  # right movement
             player.x += player_vel
-        if keys[pygame.K_w] and player.y > -1: #up movement
+        if keys[pygame.K_w] and player.y > -1:  # up movement
             player.y -= player_vel
-        if keys[pygame.K_s] and player.y + player.get_height() + 20 < HEIGHT:#down movement, buffer for healthbar
+        if keys[pygame.K_s] and player.y + player.get_height() + 20 < HEIGHT:  # down movement, buffer for healthbar
             player.y += player_vel
         if keys[pygame.K_SPACE]:
             player.shoot()
 
-        for enemy in enemies[:]:
-            enemy.move(enemy_vel)
-            enemy.move_lasers(laser_vel, player)
+#checks if game has been lost
+        if lives <= 0:
+            lost = True
 
-            if random.randrange(0, (10-level)*FPS) == 1:
-                enemy.shoot()
+            if lost:
+                lost_count += 1
+
+            if lost_count > FPS * 4:
+                run = False
+            continue
+
+#transitions levels and spawns enemy ships
+        if len(enemies) == 0:
+            transition_count += 1
+
+        if transition_count > FPS * 3:
+            level += 1
+            wave_length += 1
+            if level % 5 == 0:
+                lives += 1
+                Enemy.vel += 1
+                enemy_laser_vel += 2
+            transition_count = 0
+            for i in range(wave_length):
+                enemy = Enemy(random.randrange(50, WIDTH - 100), random.randrange(-1500, 10),
+                              random.choice(["red", "blue", "green"]))
+                enemies.append(enemy)
+
+#makes enemies move, shoot, randomizes shooting
+        for enemy in enemies:
+
+            enemy.move(enemy.vel)
+
+            if enemy.y < 0:#will move down
+                enemy.direction = True
+            if enemy.y + enemy.get_height() > HEIGHT:#will move back up
+                enemy.direction = False
+
+            enemy.move_lasers(enemy_laser_vel, player)
+
+            if level > 9:
+                if random.randrange(0, FPS) == 1:
+                    enemy.shoot()
+            if level < 10:
+                if random.randrange(0, (10 - level) * FPS) == 1:#random enemy shot tempo
+                    enemy.shoot()
 
             if collide(enemy, player):
-                player.health -= 10
+                player.health -= 20
                 enemies.remove(enemy)
 
-            if player.health <= 0:
-                lives -= 1
-                player.health = 100
 
-            elif enemy.y + enemy.get_height() > HEIGHT:
-                if lives > 0:
-                    lives -= 1
-                enemies.remove(enemy)
+
+        if player.health <= 0:#player health track (shields, w/e)
+            lives -= 1
+            player.health = 100
 
 
         player.move_lasers(-laser_vel, enemies)
 
 
+# def settings_menu():#convert to a class at some point
+#     settings_run = True
+#     while settings_run:
+#         WIN.blit(Background, (0, 0))
+#         menu_label = title_font.render("Press ENTER for Main Menu", 1, (255, 255, 255))
+#         WIN.blit(menu_label, (WIDTH / 2 - menu_label.get_width() / 2, HEIGHT / 2 - menu_label.get_height() / 2))
+#         pygame.display.update()
+#
+#         for event in pygame.event.get():
+#             if event.type == pygame.QUIT:
+#                 settings_run = False
+#             if event.type == pygame.KEYDOWN:
+#                 if event.key == pygame.K_RETURN:
+#                     main_menu()
 
-def main_menu():
-    title_font = pygame.font.SysFont("comicsans", 70)
-    run = True
-    while run:
+def main_menu():#convert to a class at some point
+    main_menu_run = True
+    while main_menu_run:
         WIN.blit(Background, (0,0))
-        title_label = title_font.render("Press ENTER to begin", 1, (255,255,255))
-        WIN.blit(title_label, (WIDTH/2 - title_label.get_width()/2, HEIGHT/2 - title_label.get_height()/2))
+        start_label = title_font.render("Press ENTER to begin", 1, (255,255,255))
+        # settings_label = title_font.render("Press 's' for settings", 1, (255,255,255))
+        WIN.blit(start_label, (WIDTH/2 - start_label.get_width()/2, HEIGHT/2 - start_label.get_height()/2))
+        # WIN.blit(settings_label, (WIDTH / 2 - settings_label.get_width() / 2, (HEIGHT / 2 - settings_label.get_height() / 2) + start_label.get_height()))
         pygame.display.update()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                run = False
-            if event.type == pygame.K_RETURN:
-                main_loop()
+                main_menu_run = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    main_loop()
+                # if event.key == pygame.K_s:
+                #     settings_menu()
     quit()
+
 
 main_menu()
--- automatetheboringstuff/tictactoe.py
+++ automatetheboringstuff/tictactoe.py
@@ -1,10 +1,15 @@
 #board is stored as a dictionary
-global theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
+theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
             'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
             'bot-L': ' ', 'bot-M': ' ', 'bot-R': ' '}
 
-#funtion to convert dictionary into visual board
+#function to replace each key value so that board is reset
+def resetboard():
+    for key in theBoard.keys():
+        theBoard[key] = ' '
+    return theBoard
 
+#function to print board. As dict key values update (aka a move is entered), the board is updated.
 def printBoard(board):
     print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
     print('-+-+-')
@@ -12,35 +17,58 @@ def printBoard(board):
     print('-+-+-')
     print(board['bot-L'] + '|' + board['bot-M'] + '|' + board['bot-R'])
 
-#winconditions stored as dictionary for reference to check if there is a winner
-global winconditions = [['top-L', 'top-M', 'top-R'], ['mid-L', 'mid-M', 'mid-R'], ['bot-L', 'bot-M', 'bot-R'],
-                 ['top-L', 'mid-L', 'bot-L'], ['top-M', 'mid-M', 'bot-M'], ['top-R', 'mid-R', 'bot-R'],
-                 ['top-L', 'mid-M', 'bot-R'], ['top-R', 'mid-M', 'bot-L']]
-
-global turn = 'X'
-
+#Main game loop
 def play_game():
+    # winconditions stored as list of lists as reference to check if there is a winner. kept within function to reset itself with each play through.
+    winconditions = [['top-L', 'top-M', 'top-R'], ['mid-L', 'mid-M', 'mid-R'], ['bot-L', 'bot-M', 'bot-R'],
+                     ['top-L', 'mid-L', 'bot-L'], ['top-M', 'mid-M', 'bot-M'], ['top-R', 'mid-R', 'bot-R'],
+                     ['top-L', 'mid-M', 'bot-R'], ['top-R', 'mid-M', 'bot-L']]
+
+    turn = 'X'
     while True:
-        game = 0
+        game = True#____block prompts user and checks validity_____
         printBoard(theBoard)
         print('Turn for ' + turn + '. Move on which space?')
         move = str(input())
         if move not in theBoard:
             print('please enter top/mid/bot-L/M/R')
             continue
-        theBoard[move] = turn
+        if theBoard[move] == ' ':
+            theBoard[move] = turn
+        else:
+            print("Space taken, choose another")
+            continue
+
+        #block runs through win condition check and places a mark where the input matches an item in each list, kinda like bingo
         index = -1
         for each in winconditions:
             index += 1
             if move in each:
                 update_wincon = each.index(move)
                 winconditions[index][update_wincon] = turn
+
+            #actual win condition check, ends game if there is a winner
             if (each[0] == 'X' and each[1] == 'X' and each[2] == 'X') or (each[0] == '0' and each[1] == '0' and each[2] == '0'):
-                game = 1
                 print(str(turn) + ' is the winner!')
+                game = False
+
+            #checks for a draw, keeps count of empty spots left
+            emptyspots = 9
+            for key in theBoard.keys():
+                if game == True and theBoard[key] != ' ':
+                    emptyspots -= 1
+            if emptyspots == 0:
+                print('''It's a draw!''')
+                game = False
+
+        #ends game if a winner or draw has occurred, prompts play again? - will restart game if y end program if n
+        if game == False:
+            replay = input("Play again? y/n: ")
+            if replay == "y":
+                resetboard()
+                play_game()
+            else:
                 break
-        if game == 1:
-            break
         if turn == 'X':
             turn = '0'
         else:
