a9f14c838bad753cb1a2a38bdeed39c85ea93302
br0steen <osteensco@gmail.com>
2021-12-05 22:39:12 -0500

    shield down visual effect added

--- Space invaders/objects.py
+++ Space invaders/objects.py
@@ -700,18 +700,18 @@ class Boss(Ship):#have separate lists for boss, boss asset, boss weapon.
 
         
     def shield_down(self):
-        self.rect = self.ship_img.get_rect(topleft=(self.x, self.y))
-        for r in range(random.randint(5, 8)):
-            rect = self.rect
-            rect.x += random.uniform(-self.ship_img.get_width(), 0)
-            rect.y += random.uniform(-self.ship_img.get_height(), 0)
-            for i in range(0, random.randint(30, 50)):
-                particle = ShieldHit(self.rect.centerx + random.uniform(-self.ship_img.get_width(), 0),
-                self.rect.centery + random.uniform(-self.ship_img.get_height(), 0)
-                )
-                self.particles.append(particle)
         self.immune = False
         self.mask = pygame.mask.from_surface(self.ship_img)
+        self.rect = self.ship_img.get_rect(topleft=(self.x, self.y))
+        for r in range(random.randint(8, 10)):
+            rect = self.rect
+            rect.x += random.uniform(-self.ship_img.get_width()/4, self.ship_img.get_width()/4)
+            rect.y += random.uniform(-self.ship_img.get_height()/4, self.ship_img.get_height()/4)
+            for i in range(0, random.randint(30, 50)):
+                particle = ShieldHit(self.rect.centerx + random.uniform(-20, 20),
+                self.rect.centery + random.uniform(-20, 20)
+                )
+                self.particles.append(particle)
         self.rect = None
 
 
